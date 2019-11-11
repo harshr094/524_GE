0c6c442fbf0461501f64af345916f5e4572e6f6f
Harsh Ranjan <harshranjan@Harshs-MacBook-Air.local>
2019-11-11 17:43:25 -0600

    GE Changes

--- GE.cc
+++ GE.cc
@@ -7,7 +7,7 @@
 #include <vector>
 #include <queue>
 #include <utility>
-#include "par-res.h"
+#include "legion_domain.h"
 
 using namespace Legion;
 using namespace std;
@@ -24,6 +24,7 @@ enum TASK_IDs
 	B_NON_LEGION_TASK_ID,
 	C_NON_LEGION_TASK_ID,
 	D_NON_LEGION_TASK_ID,
+	POPULATE_TASK_ID,
 };
 
 enum FieldId{
@@ -36,41 +37,101 @@ struct Argument
 	    Color partition_color;
 	    int top_x,top_y;
 	    int bottom_x,bottom_y;
-	    int rows,cols;
-	    Argument(int _tx, int _ty, int _bx, int _by, int _r, int _c, Color _partition){
+	    Argument(int _tx, int _ty, int _bx, int _by, Color _partition){
 	    	top_x = _tx;
 	    	top_y = _ty;
 	    	bottom_x = _bx;
 	    	bottom_y = _by;
-	    	rows = _r;
-	    	cols = _c;
 	    	partition_color = _partition;
 	    }
 };
 
+Point<2> make_point(int x, int y) { coord_t vals[2] = { x, y }; return Point<2>(vals); }
+
 void top_level_task(const Task *task, const std::vector<PhysicalRegion> &regions, Context ctx, HighLevelRuntime *runtime) {
 	int n = 10;
 	int size = (1<<n);
-	Domain domain = Domain::from_rect<2>(Rect<2>(make_point(0, 0), make_point(size - 1, size - 1)));
+	Domain domain = Domain(Rect<2>(make_point(0, 0), make_point(size - 1, size - 1)));
   	IndexSpace is = runtime->create_index_space(ctx, domain);
     FieldSpace fs = runtime->create_field_space(ctx);
     {
         FieldAllocator allocator = runtime->create_field_allocator(ctx, fs);
         allocator.allocate_field(sizeof(int), FID_X);
-        allocator.allocate_field(sizeof(int), FID_Y);
     }
     LogicalRegion lr1 = runtime->create_logical_region(ctx, is, fs);
     Color partition_color1 = 10;
-    Argument args(0,0,size-1,size-1, n, n, partition_color1);
+
+    Argument args(0,0,size-1,size-1, partition_color1);
     TaskLauncher T_launcher(GE_TASK_ID, TaskArgument(&args,sizeof(Argument)));
     T_launcher.add_region_requirement(RegionRequirement(lr1, WRITE_DISCARD, EXCLUSIVE, lr1));
     T_launcher.add_field(0, FID_X);
     T_launcher.add_field(1,FID_Y);
     runtime->execute_task(ctx, T_launcher);
+
+    Argument Printargs(0,0,size-1,size-1, partition_color1);
+    TaskLauncher Print_launcher(POPULATE_TASK_ID, TaskArgument(&Printargs,sizeof(Argument)));
+    Print_launcher.add_region_requirement(RegionRequirement(lr1, WRITE_DISCARD, EXCLUSIVE, lr1));
+    Print_launcher.add_field(0, FID_X);
+    runtime->execute_task(ctx, Print_launcher);
 }
 
 void ge_task(const Task *task, const std::vector<PhysicalRegion> &regions, Context ctx, HighLevelRuntime *runtime){
+	Argument args = task->is_index_space ? *(const Argument *) task->local_args
+    : *(const Argument *) task->args;
+    LogicalRegion lr = regions[0].get_logical_region();
+    int tx = args.top_x;
+    int ty = args.top_y;
+    int bx = args.bottom_x;
+    int by = args.bottom_y;
+ 	int midx = tx+bx;
+ 	midx/=2;
+ 	int midy = ty + by;
+ 	midy/=2;
+    DomainPointColoring coloring;
+    IndexSpace is = lr.get_index_space();
 
+    coloring[0] = Domain(Rect<2>(make_point(tx, ty), make_point(midx-1, midy-1)));
+    coloring[1] = Domain(Rect<2>(make_point(tx, midy), make_point(midx-1, by)));
+    coloring[2] = Domain(Rect<2>(make_point(midx, ty), make_point(bx, midy-1)));
+    coloring[3] = Domain(Rect<2>(make_point(midx, midy), make_point(bx, by)));
+    Rect<1>color_space = Rect<1>(0,3);
+    IndexPartition ip = runtime->create_index_partition(ctx, is, color_space, coloring, DISJOINT_KIND, args.partition_color);
+    LogicalPartition lp = runtime->get_logical_partition(ctx, lr, ip);
+    
+    LogicalRegion firstQuad = runtime->get_logical_subregion_by_color(ctx, lp, 0);
+    LogicalRegion secondQuad = runtime->get_logical_subregion_by_color(ctx, lp, 1);
+    LogicalRegion thirdQuad = runtime->get_logical_subregion_by_color(ctx, lp, 2);
+    LogicalRegion fourthQuad = runtime->get_logical_subregion_by_color(ctx, lp, 3);
+
+    Argument Aargs(tx,ty,midx-1,midy-1,args.partition_color);
+    TaskLauncher A_launcher(A_LEGION_TASK_ID, TaskArgument(&Aargs,sizeof(Argument)));
+    A_launcher.add_region_requirement(RegionRequirement(firstQuad,WRITE_DISCARD,EXCLUSIVE,firstQuad));
+    A_launcher.add_field(0,FID_X);
+    A_launcher.add_field(1,FID_Y);
+    runtime->execute_task(ctx,A_launcher);
+}
+
+
+void a_legion_task(const Task *task, const std::vector<PhysicalRegion> &regions, Context ctx, HighLevelRuntime *runtime){
+	Argument args = task->is_index_space ? *(const Argument *) task->local_args
+    : *(const Argument *) task->args;
+}
+
+void populate_task(const Task *task, const std::vector<PhysicalRegion> &regions, Context ctx, HighLevelRuntime *runtime){
+	Argument args = task->is_index_space ? *(const Argument *) task->local_args
+    : *(const Argument *) task->args;
+    const FieldAccessor<WRITE_DISCARD, int, 1> write_acc(regions[0], FID_X);
+    for(int i = args.top_x ; i <= args.bottom_x ; i++) {
+    	for(int j = args.top_y ; j <= args.bottom_y ; j++ ){
+    		write_acc[make_point(i,j)] = rand()%10;
+    	}
+    }
+    const FieldAccessor<READ_ONLY, int, 1> read_acc(regions[0], FID_X);
+    for(int i = args.top_x ; i <= args.bottom_x ; i++) {
+    	for(int j = args.top_y ; j <= args.bottom_y ; j++ ){
+    		 cout<<i<<"~"<<j<<"~"<<read_acc[make_point(i,j)]<<" ";
+    	}cout<<endl;
+    }
 }
 
 int main(int argc,char** argv){
@@ -86,6 +147,15 @@ int main(int argc,char** argv){
         registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
         Runtime::preregister_task_variant<ge_task>(registrar, "ge_level");
     }
-
+    {
+        TaskVariantRegistrar registrar(POPULATE_TASK_ID, "populate_task");
+        registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+        Runtime::preregister_task_variant<populate_task>(registrar, "populate_task");
+    }
+    {
+        TaskVariantRegistrar registrar(A_LEGION_TASK_ID, "a_legion_task");
+        registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+        Runtime::preregister_task_variant<a_legion_task>(registrar, "a_legion_task");
+    }
     return Runtime::start(argc, argv);
 }
